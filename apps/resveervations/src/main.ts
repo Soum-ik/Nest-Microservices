f0413ab034b9c61aa6635f6bed25dbb92d5550db
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2025-04-17 23:50:17 +0600

    Refactor code structure for improved readability and maintainability

--- apps/resveervations/src/main.ts
+++ apps/resveervations/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ResveervationsModule } from './resveervations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ResveervationsModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(Logger));
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
--- apps/resveervations/src/resveervations.module.ts
+++ apps/resveervations/src/resveervations.module.ts
@@ -3,17 +3,32 @@ import { ResveervationsService } from './resveervations.service';
 import { ResveervationsController } from './resveervations.controller';
 import { DatabaseModule } from '@app/common';
 import { ResveervationsRepository } from './resveervations.repository';
-import { ResveervationDocument, ResveervationSchema } from './models/resveervation.schema';
-
+import {
+  ResveervationDocument,
+  ResveervationSchema,
+} from './models/resveervation.schema';
+import { LoggerModule } from 'nestjs-pino';
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([
       {
         name: ResveervationDocument.name,
-        schema: ResveervationSchema ,
+        schema: ResveervationSchema,
       },
     ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+            colorize: true,
+            translateTime: 'SYS:standard',
+          },
+        },
+      },
+    }),
   ],
   controllers: [ResveervationsController],
   providers: [ResveervationsService, ResveervationsRepository],
