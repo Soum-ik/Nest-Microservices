5c0d8d6dc7f988ca1483667f589c0e7f71f910a7
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2025-05-05 23:59:46 +0600

    feat(payments): implement createCharge method and DTO for Stripe integration; add necessary imports and update service logic

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,27 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { CreateChargeDto } from '../dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly stripe = new Stripe(this.configService.get<string>('STRIPE_SECRET_API_KEY')!);
+  constructor(private readonly configService: ConfigService) {}
+
+  async createCharge({ card, amount }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      payment_method: paymentMethod.id,
+      confirmation_method: 'automatic',
+      confirm: true,
+    });
+
+    return paymentIntent;
   }
 }
